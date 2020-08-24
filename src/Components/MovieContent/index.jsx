b9e6bb8e591b63221ef2512397019643d3a32b0c
hanna_zaneuskaya <hanna_zaneuskaya@epam.com>
2020-08-24 21:26:50 +0300

    Implement styles for filter and sort

--- src/Components/MovieContent/index.jsx
+++ src/Components/MovieContent/index.jsx
@@ -4,24 +4,25 @@ import Wrapper from "../Wrapper";
 const MovieContent = () => (
     <div className="content">
         <Wrapper>
-            <div>
-                <ul>
-                    <li>all</li>
-                    <li>documentary</li>
-                    <li>comedy</li>
-                    <li>comedy</li>
-                    <li>horror</li>
-                    <li>crime</li>
-                </ul>
-                <div>
-                    <h4>Sort by</h4>
-                    <select>
+            <div className="content__nav">
+                <div className="content__filter">
+                    <ul className="filter">
+                        <li className="filter__item filter__item_active">all</li>
+                        <li className="filter__item">documentary</li>
+                        <li className="filter__item">comedy</li>
+                        <li className="filter__item">horror</li>
+                        <li className="filter__item">crime</li>
+                    </ul>
+                </div>
+                <div className="content__sort">
+                    <h4 className="sort__title">Sort by</h4>
+                    <select className="sort__select">
                         <option selected>Release Date</option>
                     </select>
                 </div>
             </div>
         </Wrapper>
-    </div>
+    </div >
 );
 
 export default MovieContent;
