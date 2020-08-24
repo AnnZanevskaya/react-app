c49c091bff757638250d515b41b6837f59e31eb9
hanna_zaneuskaya <hanna_zaneuskaya@epam.com>
2020-08-24 21:33:58 +0300

    Implement Sort and Filter components

---
+++ src/Components/Filter/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Filter = () => (
+    <div className="content__filter">
+        <ul className="filter">
+            <li className="filter__item filter__item_active">all</li>
+            <li className="filter__item">documentary</li>
+            <li className="filter__item">comedy</li>
+            <li className="filter__item">horror</li>
+            <li className="filter__item">crime</li>
+        </ul>
+    </div>
+)
+
+export default Filter;
---
+++ src/Components/Sort/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Sort = () => (
+    <div className="content__sort">
+        <h4 className="sort__title">Sort by</h4>
+        <select className="sort__select">
+            <option selected>Release Date</option>
+        </select>
+    </div>
+)
+
+export default Sort;
