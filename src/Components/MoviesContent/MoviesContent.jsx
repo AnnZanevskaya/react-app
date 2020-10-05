2a3aa9efc4dfe88f1a2c40ccc96d48b27df98f72
hanna_zaneuskaya <hanna_zaneuskaya@epam.com>
2020-10-05 09:16:18 +0300

    Implement useEffect hook

--- src/Components/MoviesContent/MoviesContent.jsx
+++ src/Components/MoviesContent/MoviesContent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Wrapper from "../Wrapper";
 import Filter from "../Filter";
@@ -7,19 +7,27 @@ import MovieList from "../MovieList";
 import ErrorBoundary from "../ErrorBoundary";
 import * as MovieService from "../../Services/movie-service";
 
-const MoviesContent = () => (
-    <div className="content">
-        <Wrapper>
-            <div className="content__nav">
-                <Filter />
-                <Sort />
-            </div>
+const MoviesContent = () => {
+    const [movies, setMovies] = useState([]);
 
-            <ErrorBoundary>
-                <MovieList movies={MovieService.GetAllMovies()} />
-            </ErrorBoundary>
-        </Wrapper>
-    </div >
-);
+    useEffect(() => {
+        setMovies(MovieService.getAllMovies());
+    })
+
+    return (
+        <div className="content">
+            <Wrapper>
+                <div className="content__nav">
+                    <Filter />
+                    <Sort />
+                </div>
+
+                <ErrorBoundary>
+                    <MovieList movies={movies} />
+                </ErrorBoundary>
+            </Wrapper>
+        </div >
+    )
+};
 
 export default MoviesContent;
--- src/Services/movie-service.js
+++ src/Services/movie-service.js
@@ -2,6 +2,6 @@ import {
     movies
 } from "../Constants/constants.js";
 
-export const GetAllMovies = () => {
+export const getAllMovies = () => {
     return movies;
 }
