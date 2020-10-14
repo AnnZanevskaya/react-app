ac4b7ff8a7d0863b292feaaa860f8420fac78fd1
hanna_zaneuskaya <hanna_zaneuskaya@epam.com>
2020-10-14 20:42:10 +0300

    Implement validation for add movie

--- src/Components/MovieEdit/MovieEdit.jsx
+++ src/Components/MovieEdit/MovieEdit.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch } from 'react-redux';
 import { useFormik } from 'formik';
 
@@ -10,19 +10,18 @@ import { updateMovie } from "../../Redux/actions";
 import { movieValidationSchema } from "../../Validation/validation";
 
 const MovieEdit = ({ movie, show, handleClose }) => {
-    const [movieItem, setMovieItem] = useState(movie);
     const dispatch = useDispatch();
+
+    const formik = useFormik({
+        initialValues: movie,
+        validate: movieValidationSchema,
+        onSubmit: handleSubmit
+    });
 
     function handleSubmit() {
         dispatch(updateMovie(formik.values));
         handleClose();
     }
-
-    const formik = useFormik({
-        initialValues: movieItem,
-        validate: movieValidationSchema,
-        onSubmit: handleSubmit
-    });
 
     const selectedGenres = formik.values.genres.map((elem) => { return { label: elem, value: elem } });
 
@@ -31,9 +30,9 @@ const MovieEdit = ({ movie, show, handleClose }) => {
             <Modal show={show} handleClose={handleClose}>
                 <form onSubmit={(e) => { formik.handleSubmit(); e.preventDefault(); }}>
                     <h2 className="description">Edit Movie</h2>
-                    <Input name="id" type="text" label="Movie Id" value={movieItem.id} />
+                    <Input name="id" type="text" label="Movie Id" value={movie.id} />
                     <Input name="title" type="text" label="Title" placeholder="Title here" value={formik.values.title} onChange={formik.handleChange} validationError={formik.errors.title} />
-                    <Input name="releaseDate" type="date" label="Release Date" placeholder="Select Date" value={formik.values.release_date} onChange={formik.handleChange} validationError={formik.errors.release_date} />
+                    <Input name="release_date" type="date" label="Release Date" placeholder="Select Date" value={formik.values.release_date} onChange={formik.handleChange} validationError={formik.errors.release_date} />
                     <Input name="poster_path" type="text" label="Movie URL" placeholder="Movie Url here" value={formik.values.poster_path} onChange={formik.handleChange} validationError={formik.errors.poster_path} />
                     <Input name="overview" type="text" label="Overview" placeholder="Overview here" value={formik.values.overview} onChange={formik.handleChange} validationError={formik.errors.overview} />
                     <Select name="genre" type="text" label="Genre" options={genres} preselected={selectedGenres} onSelectChange={formik.setFieldValue} validationError={formik.errors.genres} />
