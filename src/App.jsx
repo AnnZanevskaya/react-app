54097e6cf9269dccd944607796a47377b4bd8c88
hanna_zaneuskaya <hanna_zaneuskaya@epam.com>
2020-10-20 17:57:52 +0300

    404 page implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import HomePage from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
 
 export default function App() {
   return (
@@ -9,6 +10,8 @@ export default function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/search' component={HomePage} />
+        <Route path='/film/:id' component={HomePage} />
+        <Route path='*'><NotFound/></Route>
       </Switch>
     </Router>
   );
